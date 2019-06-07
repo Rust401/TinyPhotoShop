5b4a8f2626db25e4fa845b5690834af55b21f663
rust401 <zhaodonghu94@gmail.com>
2019-06-07 00:56:57 -0700

    do some more test

--- LayerTest.cpp
+++ LayerTest.cpp
@@ -6,20 +6,22 @@ using namespace dude;
 int main()
 {
     Layer l1;
-    l1.displayLayerInfo();
+    //l1.displayLayerInfo();
     l1.init();
-    std::cout<<(*l1.matrix).size()<<std::endl;
-    std::cout<<(*l1.matrix)[0].size()<<std::endl;
+    //l1.displayLayerInfo();
     Point p=(*l1.matrix)[0][0];
     printf("%d %d %d\n",p.getRed(),p.getAlpha(),p.getLayerNumbe());
-    /*
-    for(int i=0;i<1024;++i)
-        for(int j=0;j<1024;++j)
-            printf("%d %d %d\n",p.getRed(),p.getAlpha(),p.getLayerNumbe());
-            */
-    l1.displayLayerInfo();
+    l1.reInit(2,500,500,true,"baichi");
+    //l1.displayLayerInfo();
+
+    int* dude;
+    if(dude)std::cout<<dude<<std::endl;
+    dude=new int[10];
+    if(dude)std::cout<<dude<<std::endl;
+
+
 
     l1.reInit(2,500,500,true,"baichi");
-    l1.displayLayerInfo();
+    //l1.displayLayerInfo();
     return 0;
 }
